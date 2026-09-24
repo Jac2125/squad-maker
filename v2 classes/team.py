03a083edee1ba294040048052006c48535f0b0dd
Jac2125 <91581738+Jac2125@users.noreply.github.com>
2026-09-24 02:12:13 +0000

    Startig step 2

---
+++ v2 classes/team.py
@@ -0,0 +1,9 @@
+'''
+Date: 2026.09.24
+When a match is set to go, team is created.
+This module implements what "team" needs to do and represent before, during and after the match
+1. Team will requires the players info (That's it)
+2. Players info is enough as this module's objective is to create a balanced team for a game
+3. When the match starts and ohter teammates' submit their evaluation on other teammates.
+    This module will adjust the formation to show the best outcomes.
+'''
